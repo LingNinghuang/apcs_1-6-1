9610f2aa5fba946f8562eba0375122ed09261b0a
LingNinghuang <138959332+LingNinghuang@users.noreply.github.com>
2023-07-09 13:30:38 +0800

    Create ex1-6-1.cpp

---
+++ ex1-6-1.cpp
@@ -0,0 +1,11 @@
+//variables
+#include <stdio.h> 
+int main(){
+int mynum = 24;
+float myfloatnum = 11.24;
+char myletter = 'M';
+printf("%d\n", mynum);
+printf("%f\n", myfloatnum);
+printf("%c\n", myletter);
+printf("my favorite number is %d and my favorite letter is %c", mynum, myletter);
+}
